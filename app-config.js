7afbe86667a2cf6bdc30b503187ebd19108079ae
Jake Chandler <jake.chandler@lifebeacon.com>
2026-06-24 10:34:14 -0600

    Use guardian medical release as default form

--- app-config.js
+++ app-config.js
@@ -1,7 +1,7 @@
 window.PermissionSlipConfig = {
   runtimeMode: "supabase",
   projectName: "Activity Permission Slips",
-  pdfTemplateUrl: "assets/2026-trek-paperwork-packet.pdf",
+  pdfTemplateUrl: "assets/parental-or-guardian-permission-medical-release.pdf",
   supabase: {
     url: "https://sfdqctljsozhnsokevgh.supabase.co",
     anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmZHFjdGxqc296aG5zb2tldmdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyODM5NjIsImV4cCI6MjA5NDg1OTk2Mn0.Qgtk3LRyjeRu2-IDUCPFIWrQEhw7_s9aV7ps5xP_h-w",
@@ -19,7 +19,7 @@ window.PermissionSlipConfig = {
     leaderPhone: "",
     leaderEmail: "",
     ward: "",
-    pdfTemplateUrl: "assets/2026-trek-paperwork-packet.pdf",
+    pdfTemplateUrl: "assets/parental-or-guardian-permission-medical-release.pdf",
     defaultValues: {}
   }
 };
